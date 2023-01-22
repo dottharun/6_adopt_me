677938464b19c24023f4f41738992c2fc5652052
wsltharun <tharun242424@gmail.com>
2023-01-23 00:47:40 +0530

    breedlist with reactQuery not useEffect

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,38 +1,9 @@
 import { useState, useEffect } from "react";
-
-const localCache = {};
+import { useQuery } from "@tanstack/react-query";
+import fetchBreedList from "./fetchBreedList";
 
 export default function useBreedList(animal) {
-  const [breedList, setBreedList] = useState([]);
-  const [status, setStatus] = useState("unloaded");
+  const results = useQuery(["breeds", animal, fetchBreedList]);
 
-  useEffect(() => {
-    console.log(`breed call`);
-
-    if (!animal) {
-      // if no animal is selected in the select form in searchParams
-      setBreedList([]);
-    } else if (localCache[animal]) {
-      // if it exists already in localCache
-      setBreedList(localCache[animal]);
-    } else {
-      // if it does'nt exist in localCache
-      requestBreedList();
-    }
-
-    async function requestBreedList() {
-      setBreedList([]);
-      setStatus("loading");
-
-      const res = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-      );
-      const json = await res.json();
-      localCache[animal] = json.breeds || [];
-      setBreedList(localCache[animal]);
-      setStatus("loaded");
-    }
-  }, [animal]);
-
-  return [breedList, status];
+  return [results?.data?.breeds ?? [], results.status];
 }

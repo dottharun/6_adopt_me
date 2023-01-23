194a3ca29ef6690fac6cadf3ea18ae379693af76
wsltharun <tharun242424@gmail.com>
2023-01-23 22:53:02 +0530

    console log removal

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -2,7 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import fetchBreedList from "./fetchBreedList";
 
 export default function useBreedList(animal) {
-  console.log(`usebreedlist called`, animal);
   const results = useQuery(["breeds", animal], fetchBreedList);
   return [results?.data?.breeds ?? [], results.status];
 }
